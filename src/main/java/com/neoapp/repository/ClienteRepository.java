ecdc82552f14c910253bd3cf8cc068434cc1681f
Wilgner <wilgnerpr.dev@gmail.com>
2025-09-01 19:05:08 -0300

    Criado o ClienteRepository

---
+++ src/main/java/com/neoapp/repository/ClienteRepository.java
@@ -0,0 +1,19 @@
+package com.neoapp.repository;
+
+import com.neoapp.model.Cliente;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+
+    Optional<Cliente> findByCpf(String cpf);
+
+    Page<Cliente> findByNomeContainingIgnoreCase(String nome, Pageable pageable);
+
+
+
+
+}

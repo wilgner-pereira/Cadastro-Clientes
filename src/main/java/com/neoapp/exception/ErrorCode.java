5bfcb14e4560ede06893ac8592836c9aa54d510e
Wilgner <wilgnerpr.dev@gmail.com>
2025-09-02 18:11:56 -0300

    Adicionado novos ErrorCode para usuario

--- src/main/java/com/neoapp/exception/ErrorCode.java
+++ src/main/java/com/neoapp/exception/ErrorCode.java
@@ -2,9 +2,11 @@ package com.neoapp.exception;
 
 public enum ErrorCode {
     // NÃO ENCONTRADO OU CONFLITO
-    CLIENTE_NOT_FOUND("Usuário não encontrado."),
+    CLIENTE_NOT_FOUND("Cliente não encontrado."),
     CPF_ALREADY_EXISTS("Já existe cadastro com este CPF."),
     EMAIL_ALREADY_EXISTS("Já existe cadastro com este E-mail."),
+    USUARIO_ALREADY_EXISTS("Já existe cadastro com este nome."),
+    USUARIO_NOT_FOUND("Usuário não encontrado."),
 
     // REGRA DE NEGOCIO
     VALIDATION_ERROR("Erro de validação."),

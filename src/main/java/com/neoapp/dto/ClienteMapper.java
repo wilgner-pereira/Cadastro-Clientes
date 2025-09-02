7063837740dde55576d87dc85c817edf0d05933c
Wilgner <wilgnerpr.dev@gmail.com>
2025-09-02 10:54:06 -0300

    Adicionado update no mapper

--- src/main/java/com/neoapp/dto/ClienteMapper.java
+++ src/main/java/com/neoapp/dto/ClienteMapper.java
@@ -3,6 +3,7 @@ package com.neoapp.dto;
 import com.neoapp.model.Cliente;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 import org.mapstruct.factory.Mappers;
 
 import java.time.LocalDate;
@@ -13,7 +14,20 @@ public interface ClienteMapper {
 
     ClienteMapper INSTANCE = Mappers.getMapper(ClienteMapper.class);
 
+    // Converte DTO para entidade
+    @Mapping(target = "cpf", expression = "java(dto.cpf().trim())")
+    @Mapping(target = "nome", expression = "java(dto.nome().trim())")
+    @Mapping(target = "email", expression = "java(dto.email().trim())")
+    @Mapping(target = "id", ignore = true) // Ignora o ID na criação
+    @Mapping(target = "createdAt", ignore = true) // Ignora o createdAt na criação
     Cliente toEntity(ClienteRequestDTO dto);
 
     ClienteResponseDTO toDto(Cliente cliente);
+
+    @Mapping(target = "cpf", expression = "java(dto.cpf().trim())")
+    @Mapping(target = "nome", expression = "java(dto.nome().trim())")
+    @Mapping(target = "email", expression = "java(dto.email().trim())")
+    @Mapping(target = "id", ignore = true) // Nunca atualize o ID da entidade
+    @Mapping(target = "createdAt", ignore = true) // O createdAt também não deve ser alterado
+    void updateClienteFromDto(ClienteRequestDTO dto, @MappingTarget Cliente cliente);
 }
